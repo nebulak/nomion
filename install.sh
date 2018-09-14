f52b837d6a1e07a0006ac224cf87235335e26d56
nebulak <nebulak@nebulak.localhost>
2018-09-14 12:27:17 +0200

    bug fixing

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Debian, Ubuntu, Mint
+sudo apt-get update
+sudo apt-get install tor
+sudo apt-get install git
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install flask
+sudo pip3 install stem
+
+git clone https://github.com/nebulak/nomion.git
+
+echo "successfully installed nomion and it's dependencies"
+echo "To start nomion, open a terminal and type:"
+echo "sudo nomion"
--- main.py
+++ main.py
@@ -1,40 +1,57 @@
-import sys, threading, time
+#!/usr/bin/env python3
+
+import sys, threading, time, os
 from stem.control import Controller
 from stem import SocketError, UnsatisfiableRequest
 import stem.process
 from stem.util import term
 from flask import Flask
+from flask import send_from_directory
 
 import socks
 
-# //TODO: https://www.techcoil.com/blog/serve-static-files-python-3-flask/
+# Stem for hidden services: source: https://stem.torproject.org/tutorials/over_the_river.html
+# Serving static files with flask: source: https://www.techcoil.com/blog/serve-static-files-python-3-flask/
+# Using flask with tor: source: https://gist.github.com/PaulSec/ec8f1689bfde3ce9a920
 
-# source: https://gist.github.com/PaulSec/ec8f1689bfde3ce9a920
+# Set tor_cmd: source: https://stackoverflow.com/a/25069013
 WEB_PORT = 8080
 CONTROL_PORT = 7001
 SOCKS_PORT = 7000
-HIDDEN_SERVICE_DIR = '/tmp/tor/'
-
+HIDDEN_SERVICE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'hidden_service')
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'public')
+TOR_CMD = "/Applications/TorBrowser.app/Tor/tor.real"
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hello world"
-
 def start_web_app():
-    print 'Starting web app'
+    print ('Starting web app')
     app.run(port=WEB_PORT, threaded=True)
 
 def print_bootstrap_lines(line):
     if "Bootstrapped " in line:
         print(term.format(line, term.Color.BLUE))
 
+@app.route('/', methods=['GET'])
+def serve_dir_directory_index():
+    return send_from_directory(static_file_dir, 'index.html')
+
+
+@app.route('/<path:path>', methods=['GET'])
+def serve_file_in_dir(path):
+
+    if not os.path.isfile(os.path.join(static_file_dir, path)):
+        path = os.path.join(path, 'index.html')
+
+    return send_from_directory(static_file_dir, path)
+
+
 def main():
     print(term.format("Starting Tor:\n", term.Attr.BOLD))
 
     tor_process = stem.process.launch_tor_with_config(
       config = {
+        'tor_cmd': TOR_CMD,
         'SocksPort': str(SOCKS_PORT),
         'ControlPort': str(CONTROL_PORT),
         'ExitNodes': '{ru}',
@@ -52,22 +69,21 @@ def main():
         c = Controller.from_port(port=CONTROL_PORT)
         c.authenticate()
     except SocketError:
-        print 'Cannot connect to Tor control port'
+        print ('Cannot connect to Tor control port')
         sys.exit()
 
     # Create an ephemeral hidden service
     try:
-        print 'Creating hidden service'
-        result = c.create_hidden_service(HIDDEN_SERVICE_DIR, 80, target_port=8080)
-        print " * Created host: %s" % result.hostname
+        print ('Creating hidden service')
+        result = c.create_hidden_service(HIDDEN_SERVICE_DIR, 80, target_port=WEB_PORT)
+        print (" * Created host: %s" % result.hostname)
         onion = result.hostname
     except UnsatisfiableRequest:
-        print 'Cannot create ephemeral hidden service, Tor version is too old'
+        print ('Cannot create ephemeral hidden service, Tor version is too old')
         sys.exit()
     except Exception, e:
         print e
         sys.exit()
-
 
     t.join()
 if __name__ == '__main__':
